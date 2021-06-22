20c787f988270dabaf28f047d2cd07a771b5ae2b
William Cao <williamacao@gmail.com>
2021-06-22 10:43:02 -0700

    validation to user added and relationships

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,3 +1,4 @@
 class Cocktail < ApplicationRecord
-
+    has_many :ingredients
+    has_many :users, :through: ingredients
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
 
+    has_many :cocktails
+    has_many :ingredients through: cocktails
+
+    validates :email, :first_name, presence: true
+    validates :email, uniqueness: true
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
