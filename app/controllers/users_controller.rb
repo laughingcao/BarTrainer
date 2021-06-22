93a4b634ab20c24cf7728e2161039fc1987aff85
William Cao <williamacao@gmail.com>
2021-06-22 11:11:24 -0700

    some crud for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,36 @@
 class UsersController < ApplicationController
+    before_action :require_login
+    skip_before_action :require_login, only: [:new, :create]
 
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            user_session
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+        current_user.destroy
+        session[:guest_id] = nil
+        redirect_to root_path
+    end
+
+    private
+    
+    def user_params
+        params.require(:guest).permit(:email.downcase, :password, :password_confirmation)
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+
+    def user_session
+        session[:user_id] = @user.id
+    end
 end
