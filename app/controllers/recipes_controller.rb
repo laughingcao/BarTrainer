f507987f9864aca7dee4a918c9e148634abbdc8a
William Cao <williamacao@gmail.com>
2021-07-04 14:02:41 -0700

    added redirect if not logged in to user

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,2 @@
 class RecipesController < ApplicationController
-
-def new
-    @recipe = Recipe.new
-end
-
-def index
-    if params[:cocktail_id] && @cocktail = Cocktail.find_by_id(params[:cocktail_id])
-        @recipes = @cocktail.recipes
-    else
-        @error = "That Cocktail doesn't exit yet. Maybe you should create it." if params[:cocktail_id]
-        @recipes = Recipe.all
-    end
-end
-
 end
